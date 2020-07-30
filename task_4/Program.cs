2d2cba4ee752573c21b5911bcdb7a2dd7c21ec2c
orel13001 <orel13001@yandex.ru>
2020-07-30 22:57:10 +0700

    ДЗ_2

---
+++ task_4/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lim = 3;
+            int i = 0;
+            do
+            {
+                Console.Write("Введите Логин: ");
+                string login = Console.ReadLine();
+                Console.Write("Введите Пароль: ");
+                string pas = Console.ReadLine();
+
+                if (CheckAutorization(login, pas))
+                {
+                    Console.WriteLine("Добро пожаловать");
+                    break;
+                }
+                else
+                {
+                    i++;
+                    if (i < lim)
+                    {
+                        Console.WriteLine("Повторите попытку");
+                    }
+
+                }
+            } while (i < lim);
+            if (i >= lim)
+            {
+                Console.WriteLine("Вход запрещён!");
+            }
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Проверка введённого логина и пароля
+        /// </summary>
+        /// <param name="login">Логин</param>
+        /// <param name="pas">пароль</param>
+        /// <returns>Результат проверки</returns>
+        private static bool CheckAutorization(string login, string pas)
+        {
+            if (login == "root" && pas == "GeekBrains")
+            {
+                return (true);
+            }
+            else
+            {
+                return (false);
+            }
+        }
+    }
+}
---
+++ task_7/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число а: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            recurs(a,b);
+            Console.WriteLine(Sumrecurs(a, b));
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Рекурсивный вывод чисел от а до b
+        /// </summary>
+        /// <param name="a">первое число</param>
+        /// <param name="b">второе число</param>
+        private static void recurs(int a, int b)
+        {
+            if (a <= b)
+            {
+                Console.Write(a + " ");
+                recurs(a + 1, b);
+            }
+            else Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Сумма чисел от а до b
+        /// </summary>
+        /// <param name="a">первое число</param>
+        /// <param name="b">второе число</param>
+        /// <returns>Сумма чисел</returns>
+        private static int Sumrecurs(int a, int b)
+        {
+            int sum = 0;
+            if (a <= b)
+            {
+                sum = a + Sumrecurs(a + 1, b);
+                return (sum);
+            }
+            else
+            {
+                return (sum);
+            }
+
+        }
+    }
+}
