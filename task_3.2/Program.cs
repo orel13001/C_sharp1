bc658c9ca1257667d6fe53e0f27836245ae4f09f
orel13001 <orel13001@yandex.ru>
2020-08-03 07:43:22 +0700

    Задание 2

---
+++ task_3.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вводите числа. При вводе нуля производится расчёт");
+            int num;
+            int sum = 0;
+            bool flag;
+            string s;
+            do
+            {
+                s = Console.ReadLine();
+                flag = int.TryParse(s, out num);
+                if (num % 2 != 0 && num > 0)
+                {
+                    sum += num;
+                }
+            } while (s != "0");
+            Console.WriteLine("Сумма нечётных положительных чисел: " + sum);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
