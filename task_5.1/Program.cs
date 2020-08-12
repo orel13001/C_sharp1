dce5676027f8347682d52f69cc47c82f7ed3f088
orel13001 <orel13001@yandex.ru>
2020-08-12 21:09:35 +0700

    1, 2 задания пятого урока

---
+++ task_5.1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace task_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region CheckLogin
+            //Console.Write("Введите логин: ");
+            //string login = Console.ReadLine();
+            //if (login.Length >= 2 && login.Length <= 10)
+            //{
+            //    char[] chrLogin = login.ToCharArray();
+            //    bool preCheck = true;
+            //    foreach (var chr in chrLogin)
+            //    {
+            //        if (!((Char.IsDigit(chr)) || ((chr >= 'a') && (chr <= 'z')) || ((chr >= 'A') && (chr <= 'Z'))))
+            //        {
+            //            Console.WriteLine("Неверный логин");
+            //            preCheck = false;
+            //            break;
+            //        }
+            //    }
+            //    if (preCheck)
+            //    {
+            //        if (Char.IsLetter(chrLogin[0]))
+            //        {
+            //            Console.WriteLine("Логин верный");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Неверный логин");
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Неверный логин");
+            //}
+            #endregion
+
+
+            #region CheckRegExp
+
+            string pattern = @"^[A-Za-z]{1}[A-Za-z0-9]{1,9}$";
+            Regex reg = new Regex(pattern);
+            Console.Write("Введите логин: ");
+            string login = Console.ReadLine();
+            if (reg.IsMatch(login))
+            {
+                Console.WriteLine("Логин верный");
+            }
+            else
+            {
+                Console.WriteLine("Неверный логин");
+            }
+
+            #endregion
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+    
+}
---
+++ task_5.2/Message.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace task_5._2
+{
+    static class Message
+    {
+        /// <summary>
+        /// Список слов не более заданной длинны
+        /// </summary>
+        /// <param name="s">Строка поиска</param>
+        /// <param name="MaxCharCount">максимальная длинна слова</param>
+        /// <returns>Список слов</returns>
+        public static string Word_LenghtN(string s, int MaxCharCount)
+        {
+            string[] arrStr = s.Split(' ');
+            StringBuilder outStr = new StringBuilder();
+            Regex regex = new Regex(@"^[A-Za-zА-Яа-яЁё]{1," + MaxCharCount + "}$");
+            string bufStr;
+            for (int i = 0; i < arrStr.Length; i++)
+            {
+                bufStr = TrimWord(arrStr[i]);
+                
+                if (regex.IsMatch(bufStr.ToString()))
+                {
+                    if (outStr.Length == 0)
+                        outStr.Append(bufStr.ToString());
+                    else
+                    {
+                        outStr.Append(", ");
+                        outStr.Append(bufStr);
+                    }
+                }
+            }  
+            
+            return outStr.ToString();
+        }
+
+        /// <summary>
+        /// Удаление слов, заканчивающихся на заданный символ
+        /// </summary>
+        /// <param name="s">Строка поиска</param>
+        /// <param name="chr">Символ</param>
+        /// <returns>Возращаемая строка</returns>
+        public static string Delete_Word(string s, char chr)
+        {
+            string[] arrStr = s.Split(' ');
+            StringBuilder outStr = new StringBuilder();
+            Regex regex = new Regex(@"^[A-Za-zА-Яа-яЁё]{0,}" + chr.ToString() + "{1}$");
+            string bufStr;
+            for (int i = 0; i < arrStr.Length; i++)
+            {
+
+                bufStr = TrimWord(arrStr[i]);
+                if (!regex.IsMatch(bufStr.ToString()))
+                {
+                    outStr.Append(arrStr[i]);
+                    outStr.Append(" ");
+                }
+            }
+
+            return outStr.ToString();
+        }
+
+        /// <summary>
+        /// Удаление знаков пунктуации в начале и конце слова
+        /// </summary>
+        /// <param name="s">Исходное слово</param>
+        /// <returns>Слово без знаков пунктуаации</returns>
+        private static string TrimWord (string s)
+        {
+            StringBuilder bufStr = new StringBuilder(s);
+            if (Char.IsPunctuation(bufStr[bufStr.Length - 1]))
+            {
+                bufStr.Remove(bufStr.Length - 1, 1);
+            }
+            if (Char.IsPunctuation(bufStr[0]))
+            {
+                bufStr.Remove(0, 1);
+            }
+            return bufStr.ToString();
+        }
+
+        /// <summary>
+        /// Первое вхождение самого длинного слова в строке
+        /// </summary>
+        /// <param name="s">Строка поиска</param>
+        /// <returns>Самое длинное слово</returns>
+        public static string MaxLenghtWord(string s)
+        {
+            string[] arrStr = s.Split(' ');
+            StringBuilder outStr = new StringBuilder();
+            string bufStr;
+            string maxWord = "";
+            for (int i = 0; i < arrStr.Length; i++)
+            {
+
+                bufStr = TrimWord(arrStr[i]);
+                if (bufStr.Length > maxWord.Length)
+                {
+                    maxWord = bufStr;
+                }
+            }
+
+            return maxWord.ToString();
+        }
+
+        /// <summary>
+        /// Список самых длинных слов в строке
+        /// </summary>
+        /// <param name="s">Строка поиска</param>
+        /// <returns>Список слов</returns>
+        public static string MaxLenghtWords(string s)
+        {
+            string maxWord = MaxLenghtWord(s);
+            string[] arrStr = s.Split(' ');
+            string bufStr;
+            StringBuilder maxWords = new StringBuilder("");
+            for (int i = 0; i < arrStr.Length; i++)
+            {
+                bufStr = TrimWord(arrStr[i]);
+                if (bufStr.Length == maxWord.Length)
+                {
+                    if (maxWords.Length == 0)
+                    {
+                        maxWords.Append(bufStr.ToString());
+                    }
+                    else
+                    {
+                        maxWords.Append(", ");
+                        maxWords.Append(bufStr);
+                    }
+                }
+            }
+
+            return maxWords.ToString();
+        }
+
+
+
+
+
+
+    }
+
+
+}
